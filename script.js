1811715ce54df88becd27e3d4c68b7739da8788c
Dario Tino <dariotino@icloud.com>
2025-06-02 09:15:05 +0200

    Enable 3D logo parallax effect

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 window.addEventListener("scroll", () => {
   const scrollY = window.scrollY;
-  const heroImage = document.querySelector(".hero-image");
-  heroImage.style.transform = `translateY(${scrollY * 0.2}px)`;
+
+  document.querySelector(".layer1").style.transform = `translate(-50%, calc(-50% + ${scrollY * 0.1}px))`;
+  document.querySelector(".layer2").style.transform = `translate(-50%, calc(-50% + ${scrollY * 0.2}px))`;
+  document.querySelector(".layer3").style.transform = `translate(-50%, calc(-50% + ${scrollY * 0.3}px))`;
 });
