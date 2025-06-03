f5a81d4c20b1c1b423f64116b466ef849f766243
Dario Tino <dariotino@icloud.com>
2025-06-03 22:10:10 +0200

    Publish site

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-document.addEventListener("mousemove", (e) => {
-  const layers = document.querySelectorAll(".layer");
-  layers.forEach((layer, index) => {
-    const speed = (index + 1) * 5;
-    const x = (window.innerWidth - e.pageX * speed) / 100;
-    const y = (window.innerHeight - e.pageY * speed) / 100;
-    layer.style.transform = `translate(-50%, -50%) translate(${x}px, ${y}px)`;
+document.addEventListener("scroll", () => {
+  const scrollY = window.scrollY;
+  document.querySelectorAll(".parallax").forEach(el => {
+    const speed = parseFloat(el.dataset.speed);
+    const offset = parseFloat(el.dataset.offset || 0);
+    el.style.transform = `translate(-50%, ${offset + scrollY * speed}px)`;
   });
 });
