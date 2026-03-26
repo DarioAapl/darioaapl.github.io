9648234b666039c17d4276e57a29ecd465517b9d
DarioAapl <dariotino@icloud.com>
2026-03-26 02:19:16 +0100

    update

--- test_flags.py
+++
@@ -1,6 +0,0 @@
-with open("index.html", "r") as f:
-    text = f.read()
-import re
-flags = re.findall(r'<a.*?class="amz-btn".*?>.*?Amazon (.*?)</a>', text, re.DOTALL)
-for f in flags:
-    print(f.strip())
